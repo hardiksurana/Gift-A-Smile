a121ae36d8458b0c0e4d597693e012a168a13c0c
hardiksurana <hardiksurana01@gmail.com>
2016-12-27 12:30:43 +0530

    updated milestones

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Gift-A-Smile
-Gift A Smile is a not-for-profit initiative to crowdsource funds for helping children under the age of 11 of an orphanage. You can view the site on www.giftasmile.github.io
+Gift A Smile is a not-for-profit initiative to crowdsource funds for helping children under the age of 11 of an orphanage.
+[Gift A Smile](https://hardiksurana.github.io/Gift-A-Smile/)
--- js/main.js
+++ js/main.js
@@ -127,3 +127,17 @@ function initializeClock(id, endtime) {
 
 var deadline = new Date(Date.parse("December 25, 2016"));
 initializeClock('clockdiv', deadline);
+
+
+// for achievements
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
